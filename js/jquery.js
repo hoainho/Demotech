ac312ecb5bbaa745f8db9474082d1c8c65e9e615
Hoài Nhớ <47898976+hoainho@users.noreply.github.com>
2020-04-25 16:19:21 +0700

    Done 2 components
    Check me

---
+++ js/jquery.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+
+    var active1 = false;
+    var active2 = false;
+    var active3 = false;
+    var active4 = false;
+  
+      $('.nav-bg--per').on('mousedown touchstart', function() {
+      
+      if (!active1) $(this).find('.nav-bg--per__icon1').css({'background-color': 'black', 'transform': 'translate(0px,125px)'});
+      else $(this).find('.nav-bg--per__icon1').css({'background-color': 'dimGray', 'transform': 'none'}); 
+       if (!active2) $(this).find('.nav-bg--per__icon2').css({'background-color': 'black', 'transform': 'translate(60px,105px)'});
+      else $(this).find('.nav-bg--per__icon2').css({'background-color': 'darkGray', 'transform': 'none'});
+        if (!active3) $(this).find('.nav-bg--per__icon3').css({'background-color': 'black', 'transform': 'translate(105px,60px)'});
+      else $(this).find('.nav-bg--per__icon3').css({'background-color': 'silver', 'transform': 'none'});
+        if (!active4) $(this).find('.nav-bg--per__icon4').css({'background-color': 'black', 'transform': 'translate(125px,0px)'});
+      else $(this).find('.nav-bg--per__icon4').css({'background-color': 'silver', 'transform': 'none'});
+      active1 = !active1;
+      active2 = !active2;
+      active3 = !active3;
+      active4 = !active4;
+        
+      });
+  });
+
+
+////Time
+function clock(){
+  var hours = document.getElementById('hour');
+  var minutes = document.getElementById('minutes');
+  var seconds = document.getElementById('seconds');
+  
+  var h = new Date().getHours();
+  var m = new Date().getMinutes();
+  var s = new Date().getSeconds();
+
+  hours.innerHTML = h;
+  minutes.innerHTML = m;
+  seconds.innerHTML = s;
+}
+  var interval = setInterval(clock,1000);
+  
